d293746fd362f9fafbcc428dfa95dd38d7b836a5
KyungJunNoh <developerjun0615@gmail.com>
2024-09-17 21:32:51 +0900

    feat: Hello world
    - Kotlin, Spring Boot 설정
    - spring web, msa 기본 설정
    - mariadb connect 설정
    - 공통 의존성 설정
    
    etc..

---
+++ src/test/kotlin/com/nfu/fetch/FetchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nfu.fetch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FetchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
